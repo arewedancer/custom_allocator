9137b7823fc9ba70fbb6b726871e93f7d6ba2f9e
arewedancer <arewedancer111@gmail.com>
2019-05-29 13:52:38 -0500

    mprotect allocator for vector and hopscotch_map

--- align_alloc.hpp
+++ align_alloc.hpp
@@ -1,48 +1,81 @@
 #include <cstddef>
 #include <cstdio>
 #include <fcntl.h>
-#include <sys/mman.h>
+#include <iostream>
+#include <malloc.h>
 #include <stdlib.h>
+#include <sys/mman.h>
 #include <unistd.h>
-template <class T>
-struct AlignAllocator {
-	typedef T value_type;
-	T* _addr; std::size_t _size;
-	inline explicit AlignAllocator(){};
-	template <class U> AlignAllocator(const AlignAllocator<U>& other){};
-	T* allocate(std::size_t n){
-		//int pagesize = sysconf(_SC_PAGE_SIZE);
-		int pagesize = 4096;
-		_size = n * sizeof(T);
-		posix_memalign((void**)&_addr, pagesize, n * sizeof(T));
-		return reinterpret_cast<T*>(_addr);
-		//return reinterpret_cast<T*>(alloca(n * sizeof(T))); 
-	};
-	void deallocate(T* p, std::size_t n)
-	{
-		//munmap(p, n * sizeof(T)); 
-		unprotect();
-		free(p);
-	};
-  int read_protect()
-	{
-		int ret = mprotect((void*)_addr, _size, PROT_READ);
-		return ret;
-	};
-  int unprotect()
-	{
-		int ret = mprotect((void*)_addr, _size, PROT_READ|PROT_WRITE|PROT_EXEC);
-		return ret;
-	};
-};
-template <class T, class U>
-bool operator==(const AlignAllocator<T>&, const AlignAllocator<U>&)
-{
-	return false;;
-};
-template <class T, class U>
-bool operator!=(const AlignAllocator<T>&, const AlignAllocator<U>&)
-{
-	return true;
+template <typename T> struct AlignAllocator {
+  using value_type = T;
+  using pointer = T *;
+  using const_pointer = const T *;
+  using reference = T &;
+  using const_reference = const T &;
+  using size_type = std::size_t;
+
+  static void *_addr;
+  static std::size_t _size;
+  inline explicit AlignAllocator() {};
+
+  template <typename U> struct rebind { using other = AlignAllocator<U>; };
+
+  template <class U> AlignAllocator(const AlignAllocator<U> &other) {
+    _size = other._size;
+    _addr = other._addr;
+  };
+
+  pointer address(reference x) const noexcept { return &x; }
+
+  const_pointer address(const_reference x) const noexcept { return &x; }
+
+  T *allocate(std::size_t n) {
+    int pagesize = sysconf(_SC_PAGE_SIZE);
+    if (n > 1) {
+      size_t ex = n * sizeof(T);
+      size_t pg = 1;
+      while (1) {
+        if (pg * pagesize / ex > 1)
+          break;
+        pg++;
+      };
+      posix_memalign((void **)&_addr, pagesize, pg * pagesize);
+      _size = n * sizeof(T);
+    } else {
+      return reinterpret_cast<T *>(malloc(_size * sizeof(T)));
+    };
+    return reinterpret_cast<T *>(_addr);
+  };
+  void deallocate(T *p, std::size_t n) {
+    if (n > 1)
+      unprotect(p, n);
+    free(p);
+  };
+
+  int read_protect() { return mprotect((void *)_addr, _size, PROT_READ); };
+
+  int unprotect(void *p, std::size_t n) {
+    return mprotect((void *)p, n * sizeof(T),
+                    PROT_READ | PROT_WRITE | PROT_EXEC);
+  };
+
+  template <typename U, typename... Args>
+  void construct(U *p, Args &&... args) {
+    ::new (static_cast<void *>(p)) U(std::forward<Args>(args)...);
+  }
+
+  template <typename U> void destroy(U *p) { p->~U(); }
 };
 
+template <class T> void *AlignAllocator<T>::_addr = 0x0;
+template <class T> std::size_t AlignAllocator<T>::_size = 0;
+
+template <class T, class U>
+bool operator==(const AlignAllocator<T> &, const AlignAllocator<U> &) {
+  return false;
+  ;
+};
+template <class T, class U>
+bool operator!=(const AlignAllocator<T> &, const AlignAllocator<U> &) {
+  return true;
+};
--- test.cpp
+++ test.cpp
@@ -3,27 +3,45 @@
 #include <memory>
 #include <vector>
 #include <map>
+#include <functional>
 #include <ext/debug_allocator.h>
 #include <ext/pool_allocator.h>
 #include <ext/mt_allocator.h>
 #include "shm_allocator.hpp"
 #include "sta_allocator.hpp"
 #include "align_alloc.hpp"
+#include "tsl/hopscotch_hash.h"
+#include "tsl/hopscotch_map.h"
+#include "utils.h"
 
-#define max 100000
+#define max 10000
 int main()
 {
 //std::vector<int, ShmAllocator<int> > test_vector;
 //std::vector<int, __gnu_cxx::__pool_alloc<int> > test_vector;
 //std::vector<int, StaAllocator<int> > test_vector;
-std::vector<int, AlignAllocator<int> > test_vector;
+//std::vector<int, AlignAllocator<int> > test_vector;
 //std::vector<int, __gnu_cxx::__mt_alloc<int> > test_vector;
-test_vector.reserve(max);
+//test_vector.reserve(max);
+//std::map<int, int, std::equal_to<int>, AlignAllocator<std::pair<const int, int>>> test_map;
+	tsl::hopscotch_map<int, int, mod_hash<9>, std::equal_to<int>, AlignAllocator<std::pair<const int,int>>> test_map;
 for (int i = 0; i < max; ++i )
-	test_vector.push_back(i);
-
-test_vector.get_allocator().read_protect();
-std::cout << test_vector[3] << std::endl;
-test_vector[3] = 123;
+{
+		test_map.insert(std::pair<int,int>(i,i*3));
+		//test_vector.push_back(i);
+};
+test_map.get_allocator().read_protect();
+for(auto s : test_map )
+{
+	std::cout << s.first << ":" << s.second << std::endl;
+};
+test_map.get_allocator().read_protect();
+test_map[2]=12;
+//for (int i = 0; i < max; ++i )
+//{
+//std::cout << test_vector[i] << std::endl;
+//std::cout << test_map[i] << std::endl;
+//};
+//test_vector[3] = 123;*/
 
 };
